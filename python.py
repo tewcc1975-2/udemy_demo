37ae545989a7ba4aa1bf4c4feeb6f9d197c3d40a
tewcc1975-2 <tewcc@yahoo.com.tw>
2023-02-02 01:23:01 +0800

    feature-001 changes

--- python.py
+++ python.py
@@ -1,1 +1,20 @@
+This is a change from feature branch
+
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+
+This is annotated tag
+This is annotated tag
+This is annotated tag
+This is annotated tag
+
 This is a test script
+
+this is version 1
+
+This is ber vesion 2
+This is ber vesion 2
+This is ber vesion 2
---
+++ terraform.tf
@@ -0,0 +1,18 @@
+This is a change from feature branch
+
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+This is lightweighted tag
+
+This is annotated tag
+This is annotated tag
+This is annotated tag
+This is annotated tag
+
+this is version 1
+
+This is ber vesion 2
+This is ber vesion 2
+This is ber vesion 2
